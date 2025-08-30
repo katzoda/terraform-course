5989428e081de6c4b8d421d35783beec9c479652
Daniel Katzor <69030416+katzoda@users.noreply.github.com>
2025-08-30 19:50:25 +0000

    Add readme and notes

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -4,3 +4,13 @@ resource "github_repository" "kd-repo" {
   visibility  = "private"
   auto_init   = true
 }
+
+resource "github_repository_file" "readme" {
+  # it is setting an implicit tenancy - this github_repository needs to be created first
+  # because I have specified the implicit reference or "dependency link" to the repository and didn't hardcoded the name "kd-repo" >> terraform will know that it needs to create this repo first
+  repository = github_repository.kd-repo.name
+  branch = "main"
+  file = "README.md"
+  content = "# This is a repo for infra developers"
+  overwrite_on_create = true
+}
