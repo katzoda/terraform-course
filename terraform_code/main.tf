b835082a48cb4bcc94482c2734560b08080de818
Daniel Katzor <69030416+katzoda@users.noreply.github.com>
2025-09-06 17:20:34 +0000

    Improve comments

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -13,7 +13,6 @@ resource "github_repository_file" "readme" {
   # by specifying repository attribute as "github_repository.kd-repo.name", it is setting an implicit tenancy or "dependency link"
   # terraform will then know that it needs to create this repo first
   repository = github_repository.kd-repo.name
-  repository = github_repository.kd-repo.name
   branch = "main"
   file = "README.md"
   content = "# This is a repo for infra developers"
